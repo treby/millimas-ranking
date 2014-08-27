4d3b9dacc5939184c27d5ec7315d7f1fdccf26f9
treby <treby@atelier-nodoka.net>
2014-08-28 03:08:54 +0900

    Migrate html for highcharts.

--- extension/web/js/index.js
+++ extension/web/js/index.js
@@ -1,9 +1,45 @@
-window.onload = function() {
-    g = new Dygraph(
-        // containing div
-        document.getElementById("graphdiv"),
-
-        // path to csv file.
-        'data/sample.csv'
-    );
-}
+$(function() {
+    $.get('data/sample.csv', function (data) {
+        var options = {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'ranking border'
+            },
+            xAxis: {
+                categories: []
+            },
+            yAxis: {
+                title: {
+                    text: 'pt'
+                },
+            },
+            series: []
+        };
+        var lines = data.split('\n');
+        $.each(lines, function(lineNo, line) {
+            var items = line.split(',');
+            if (lineNo == 0) {
+                $.each(items, function(itemNo, item) {
+                    var series = {
+                        data: []
+                    };
+                    if (itemNo > 0) {
+                        series.name = item;
+                        options.series.push(series);
+                    }
+                });
+            } else if(items.length == 4) {
+                $.each(items, function(itemNo, item) {
+                    if (itemNo == 0) {
+                        options.xAxis.categories.push('');
+                    } else {
+                        options.series[itemNo - 1].data.push(parseInt(item));
+                    }
+                });
+            }
+        });
+        $('#container').highcharts(options);
+    });
+});
