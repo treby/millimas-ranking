2698c57fd8c696c7ae6a5e8a6726b862c5670944
treby <treby@atelier-nodoka.net>
2014-10-07 02:19:50 +0900

    Tweet with velocity.

--- server/tweet_bot.rb
+++ server/tweet_bot.rb
@@ -1,4 +1,16 @@
 require 'twitter'
+require 'influxdb'
+require 'time'
+require 'optparse'
+
+# for border speed.
+params = ARGV.getopts('s:f:')
+series_name = 'sample'
+series_name = params['s'] unless params['s'].nil?
+host = 'localhost'
+user = 'treby'
+pass = 'treby'
+db_name = 'millimas_ranking'
 
 def parse(packet)
   meta_data = packet.pop
@@ -30,13 +42,19 @@ end
 
 exit if data.nil?
 
+influxdb = InfluxDB::Client.new db_name, host: host, username: user, password: pass
 timestamp = data[:updated_at]
+ret = influxdb.query "SELECT * FROM #{series_name} WHERE time > '#{(timestamp - 60 * 60).utc.strftime("%Y-%m-%d %H:%M:%S")}'"
+past_data = ret[series_name].last
 
-tweet = "#{timestamp.month}月#{timestamp.day}日 #{timestamp.hour}時#{timestamp.min}分時点のボーダーは\n"
+border_tweet = "#{timestamp.month}月#{timestamp.day}日 #{timestamp.hour}時#{timestamp.min}分時点のボーダーは\n"
+velocity_tweet = "#{timestamp.month}月#{timestamp.day}日 #{timestamp.hour}時#{timestamp.min}分時点の大体の時速は\n"
 data.select{|sym| sym.to_s.include? 'border_' }.each do |border, score|
-  tweet += "  #{border.to_s.sub('border_', '')}位 #{score.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse}pt\n"
+  border_tweet += "  #{border.to_s.sub('border_', '')}位 #{score.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse}pt\n"
+  velocity_tweet += "  #{border.to_s.sub('border_', '')}位 #{(score - past_data[border.to_s]).to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse}pt/h\n"
 end
-tweet += "です。\n"
+border_tweet += "です。\n"
+velocity_tweet += "です。\n"
 
 Twitter::REST::Client.new(
   consumer_key:        ENV['TWITTER_CONSUMER_KEY'],
@@ -44,5 +62,6 @@ Twitter::REST::Client.new(
   access_token:        ENV['TWITTER_ACCESS_TOKEN'],
   access_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET']
 ) do |bot|
-  bot.update tweet
+  bot.update border_tweet
+  bot.update velocity_tweet
 end
